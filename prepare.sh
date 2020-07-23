51aa692051755464481fa45d1b31a8af34a6050a
yabosun <yabosun@163.com>
2020-07-23 16:28:29 +0800

    update data process

---
+++ prepare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pip install pandas --no-cache-dir -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
+pip install nltk --no-cache-dir -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
+pip install tables --no-cache-dir -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
